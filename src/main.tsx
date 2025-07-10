44223936c031033b1a9e9476a677ff189a42ea7b
Øystein Knudsen <oysteinknudsen96@gmail.com>
2025-07-10 13:18:23 +0200

    Revert "Analytics"
    This reverts commit 9552330887d8e577aa592421c6b4bd3820006ffa.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import "./index.css";
 import App from "./App.tsx";
 import React from "react";
 import { Provider } from "./components/ui/provider.tsx";
-import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
       <App />
-      <Analytics />
     </Provider>
   </React.StrictMode>
 );
