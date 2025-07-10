9552330887d8e577aa592421c6b4bd3820006ffa
Øystein Knudsen <oysteinknudsen96@gmail.com>
2025-07-10 13:13:02 +0200

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import "./index.css";
 import App from "./App.tsx";
 import React from "react";
 import { Provider } from "./components/ui/provider.tsx";
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
       <App />
+      <Analytics />
     </Provider>
   </React.StrictMode>
 );
