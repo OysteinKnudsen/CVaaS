e521891a5825cf60ee8e91763f001bfa1991346a
Øystein Knudsen <oysteinknudsen96@gmail.com>
2025-07-11 09:45:07 +0200

    Trigger inline browser download

--- src/components/PhotoGrid/ImageVariant/DownloadPhotoButton.tsx
+++ src/components/PhotoGrid/ImageVariant/DownloadPhotoButton.tsx
@@ -1,22 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { Button, DownloadTrigger } from "@chakra-ui/react";
 import { RiDownload2Line } from "react-icons/ri";
 
 interface Props {
-    downloadUrl: string;
+  downloadUrl: string;
 }
 
 export const DownloadPhotoButton: React.FC<Props> = (props) => {
-
-const handleDownload = () => {
-    const a = document.createElement("a");
-    a.href = props.downloadUrl;
-    a.download = "image.png";
-    a.click();
-}
+  const fetchImage = async () => {
+    const response = await fetch(props.downloadUrl);
+    const blob = await response.blob();
+    return blob;
+  };
 
   return (
-    <Button variant={"ghost"} color={"white"} size={"md"} onClick={handleDownload}>
-      <RiDownload2Line/>
-    </Button>
+    <DownloadTrigger
+      data={fetchImage}
+      fileName={"photo_variant"}
+      mimeType={"image/*"}
+    >
+      <Button
+        variant={"ghost"}
+        color={"white"}
+        size={"md"}
+      >
+        <RiDownload2Line />
+      </Button>
+    </DownloadTrigger>
   );
 };
